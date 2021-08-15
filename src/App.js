0e1449625f826e6d9c646b7c3c506a82d03dab47
Wylie Fisher <wylie@dukeofcheese.com>
2021-08-15 19:39:54 -0400

    fix the render for App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 
 import { StyledDiv } from "./styled.js";
 
-class App extends React.Component {
-  render() {
-    return (
-      <StyledDiv>
-        <div class="back">
-          <a href="http://www.fisherillustration.com" class="anchor illustration"><span class="imagewrap"></span></a>
-          <a href="http://www.particle17.com" class="anchor comix"><span class="imagewrap"></span></a>
-        </div>
-      </StyledDiv>
-    )
-  }
-}
+const App = ({ children }) => (
+  <StyledDiv>
+    <div class="back">
+      <a href="http://www.fisherillustration.com" class="anchor illustration"><span class="imagewrap"></span></a>
+      <a href="http://www.particle17.com" class="anchor comix"><span class="imagewrap"></span></a>
+    </div>
+  </StyledDiv>
+);
 
 export default App;
