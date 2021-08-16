33924643eb6b0a0211ff9ad3b062ef4f4921aa2b
Wylie Fisher <wylie@dukeofcheese.com>
2021-08-16 11:33:48 -0400

    bah, className not class

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Wrap, Back, Anchor, ImageWrap } from "./styled.js";
 const App = ({ children, wrap }) => (
   <Wrap>
     <Back>
-      <Anchor href="http://www.fisherillustration.com" class="illustration" target="_blank">
+      <Anchor href="http://www.fisherillustration.com" className="illustration" target="_blank">
         <ImageWrap className="illustration" />
       </Anchor>
-      <Anchor href="http://www.particle17.com" class="comix" target="_blank">
+      <Anchor href="http://www.particle17.com" className="comix" target="_blank">
         <ImageWrap className="comix" />
       </Anchor>
     </Back>
